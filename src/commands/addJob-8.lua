7edd8f47b392c8b3a7369196befdafa4b29421d1
Manuel Astudillo <manuel@optimalbits.com>
2019-10-13 13:59:32 +0200

    fix: do not trim if maxEvents is undefined

--- src/commands/addJob-8.lua
+++ src/commands/addJob-8.lua
@@ -101,6 +101,6 @@ else
 end
 
 local maxEvents = rcall("HGET", KEYS[3], "opts.maxLenEvents")
-rcall("XTRIM", KEYS[7], "MAXLEN", "~", maxEvents)
+if (maxEvents ~= nil) then rcall("XTRIM", KEYS[7], "MAXLEN", "~", maxEvents) end
 
 return jobId .. "" -- convert to string
