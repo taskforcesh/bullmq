bad9814c8406394a090e7a73d5f5c6e18c2d8eea
Robert D'Ercole <bobdercole@gmail.com>
2020-02-12 13:27:32 -0500

    fix: fixed exit code 12 when using inspect with child process

--- src/classes/child-pool.ts
+++ src/classes/child-pool.ts
@@ -63,7 +63,7 @@ export class ChildPool {
       }
     }
 
-    child = fork(masterFile, execArgv);
+    child = fork(masterFile, [], { execArgv });
     child.processFile = processFile;
 
     _this.retained[child.pid] = child;
