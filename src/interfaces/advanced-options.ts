23aadc300b947693f4afb22296d236a924bd11ca
Joe Bowbeer <joe.bowbeer@gmail.com>
2020-06-04 13:03:53 -0700

    fix: remove unused options

--- src/interfaces/advanced-options.ts
+++ src/interfaces/advanced-options.ts
@@ -1,28 +1,8 @@
 export interface AdvancedOptions {
-  // How often check for stalled jobs (use 0 for never checking).
-  stalledInterval?: number;
-
-  // Max amount of times a stalled job will be re-processed.
-  maxStalledCount?: number;
-
-  // Poll interval for delayed jobs and added jobs.
-  guardInterval?: number;
-
-  // delay before processing next job in case of internal error.
-  retryProcessDelay?: number;
-
   // A set of custom backoff strategies keyed by name.
   backoffStrategies?: {};
-
-  // A timeout for when the queue is in drained state (empty waiting for jobs).
-  drainDelay?: number;
 }
 
 export const AdvancedOptionsDefaults: AdvancedOptions = {
-  stalledInterval: 30000,
-  maxStalledCount: 1,
-  guardInterval: 5000,
-  retryProcessDelay: 5000,
   backoffStrategies: {},
-  drainDelay: 5,
 };
