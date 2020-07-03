18a00b80779b2e8b402149b7376ad41ea1fcc57d
Manuel Astudillo <manuel@optimalbits.com>
2020-07-03 19:45:52 +0200

    chore: add global event types

--- src/classes/queue-events.ts
+++ src/classes/queue-events.ts
@@ -4,13 +4,40 @@ import { QueueBase } from './queue-base';
 
 export declare interface QueueEvents {
   on(
+    event: 'waiting',
+    listener: (args: { jobId: string }, id: string) => void,
+  ): this;
+  on(
+    event: 'delayed',
+    listener: (args: { jobId: string; delay: number }, id: string) => void,
+  ): this;
+  on(
+    event: 'progress',
+    listener: (args: { jobId: string; data: string }, id: string) => void,
+  ): this;
+  on(
+    event: 'stalled',
+    listener: (args: { jobId: string }, id: string) => void,
+  ): this;
+  on(
     event: 'completed',
-    listener: (args: { jobId: string; returnvalue: any }, id: string) => void,
+    listener: (
+      args: { jobId: string; returnvalue: string; prev?: string },
+      id: string,
+    ) => void,
   ): this;
   on(
     event: 'failed',
-    listener: (args: { jobId: string; failedReason: any }, id: string) => void,
+    listener: (
+      args: { jobId: string; failedReason: string; prev?: string },
+      id: string,
+    ) => void,
   ): this;
+  on(
+    event: 'removed',
+    listener: (args: { jobId: string }, id: string) => void,
+  ): this;
+  on(event: 'drained', listener: (id: string) => void): this;
   on(event: string, listener: Function): this;
 }
 
