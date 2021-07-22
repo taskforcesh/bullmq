c465611ed76afd2adfd0e05a8babd6e369f5c310
Neal Granger <neal@nealg.com>
2021-07-22 15:06:59 -0700

    fix: connection hangs with failed connection fixes #656

--- src/classes/redis-connection.ts
+++ src/classes/redis-connection.ts
@@ -6,6 +6,11 @@ import { load } from '../commands';
 import { ConnectionOptions, RedisOptions } from '../interfaces';
 import { isRedisInstance } from '../utils';
 
+/**
+ * See https://github.com/luin/ioredis/blob/d65f8b2/lib/utils/index.ts#L338
+ */
+const CONNECTION_CLOSED_ERROR_MSG = 'Connection is closed.';
+
 export type RedisClient = Redis | Cluster;
 
 export class RedisConnection extends EventEmitter {
@@ -48,29 +53,31 @@ export class RedisConnection extends EventEmitter {
    * @param {Redis} redis client
    */
   static async waitUntilReady(client: RedisClient) {
-    return new Promise<void>(function(resolve, reject) {
-      if (client.status === 'ready') {
+    if (client.status === 'ready') {
+      return;
+    }
+
+    if (client.status === 'wait') {
+      return client.connect();
+    }
+
+    if (client.status === 'end') {
+      throw new Error(CONNECTION_CLOSED_ERROR_MSG);
+    }
+
+    return new Promise<void>((resolve, reject) => {
+      const handleReady = () => {
+        client.removeListener('end', endHandler);
         resolve();
-      } else {
-        const handleError = function(err: NodeJS.ErrnoException) {
-          if (err['code'] !== 'ECONNREFUSED') {
-            client.removeListener('ready', handleReady);
-            reject(err);
-          }
-        };
+      };
 
-        const handleReady = async function() {
-          client.removeListener('error', handleError);
-          resolve();
-        };
+      const endHandler = () => {
+        client.removeListener('ready', handleReady);
+        reject(new Error(CONNECTION_CLOSED_ERROR_MSG));
+      };
 
-        client.once('ready', handleReady);
-        client.once('error', handleError);
-        
-        if (client.status === 'wait') {
-          client.connect(); 
-        }
-      }
+      client.once('ready', handleReady);
+      client.once('end', endHandler);
     });
   }
 
@@ -130,7 +137,13 @@ export class RedisConnection extends EventEmitter {
     if (!this.closing) {
       this.closing = true;
       if (this.opts != this._client) {
-        await this._client.quit();
+        try {
+          await this._client.quit();
+        } catch (error) {
+          if (error.message !== CONNECTION_CLOSED_ERROR_MSG) {
+            throw error;
+          }
+        }
       } else {
         this._client.off('error', this.handleClientError);
       }
