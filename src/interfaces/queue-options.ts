f63502dc1b538e51e658a1d01a301334baedf5b7
Manuel Astudillo <manuel@optimalbits.com>
2021-08-24 14:51:28 +0800

    docs(queue): improved queue docs

--- src/interfaces/queue-options.ts
+++ src/interfaces/queue-options.ts
@@ -6,32 +6,64 @@ export enum ClientType {
   normal = 'normal',
 }
 
+/**
+ * Base Queue options
+ */
 export interface QueueBaseOptions {
   connection?: ConnectionOptions;
+
   /**
    * Prefix for all queue keys.
    */
   prefix?: string;
 }
 
+/**
+ * Options for the Queue class.
+ */
 export interface QueueOptions extends QueueBaseOptions {
   defaultJobOptions?: JobsOptions;
 
+  /**
+   * Options for the rate limiter.
+   */
   limiter?: {
+    /**
+     * Group key to be used by the limiter when
+     * limiting by group keys.
+     */
     groupKey: string;
   };
 
+  /**
+   * Options for the streams used internally in BullMQ.
+   */
   streams?: {
+    /**
+     * Options for the events stream.
+     */
     events: {
       /**
-       * Max approximated length for streams
+       * Max approximated length for streams. Default is 10 000 events.
        */
       maxLen: number;
     };
   };
 }
 
+/**
+ * Options for QueueEvents
+ */
 export interface QueueEventsOptions extends QueueBaseOptions {
+  /**
+   * Last event Id. If provided it is possible to continue
+   * consuming events from a known Id instead of from the last
+   * produced event.
+   */
   lastEventId?: string;
+
+  /**
+   * Timeout for the blocking XREAD call to the events stream.
+   */
   blockingTimeout?: number;
 }
