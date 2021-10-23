eb1bc670000d1e68e5adeb5117b9af7deb65843a
rogger andré valverde flores <rogger.valverde@uni.pe>
2021-10-22 23:07:25 -0500

    test(flow): clean data after tests

--- src/test/test_flow.ts
+++ src/test/test_flow.ts
@@ -25,7 +25,7 @@ describe('flows', () => {
 
   afterEach(async function() {
     await queue.close();
-    //await removeAllQueueData(new IORedis(), queueName);
+    await removeAllQueueData(new IORedis(), queueName);
   });
 
   it('should process children before the parent', async () => {
