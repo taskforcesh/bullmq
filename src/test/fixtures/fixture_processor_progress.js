ee5c37b08b50f25a8527aacfb71d8ae8c9a2f8d4
Rogger Valverde <rogger.valverde@uni.pe>
2021-11-30 21:57:37 -0500

    fix(sandboxed-process): delete progress function (#846)
    BREAKING CHANGE: replacement of progress function by updateProgress

--- src/test/fixtures/fixture_processor_progress.js
+++ src/test/fixtures/fixture_processor_progress.js
@@ -10,22 +10,18 @@ module.exports = function(job) {
   return delay(50)
     .then(() => {
       job.updateProgress(10);
-      job.log(job.progress());
       return delay(100);
     })
     .then(() => {
-      job.progress(27);
-      job.log(job.progress());
+      job.updateProgress(27);
       return delay(150);
     })
     .then(() => {
       job.updateProgress(78);
-      job.log(job.progress());
       return delay(100);
     })
     .then(() => {
-      job.progress(100);
-      job.log(job.progress());
+      job.updateProgress(100);
     })
     .then(() => {
       return 37;
