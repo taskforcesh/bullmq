6442c63dec653bfe200bb9467dbf88c72909b862
rogger andré valverde flores <rogger.valverde@uni.pe>
2021-12-22 18:46:24 -0500

    refactor: consider directory copy

--- commandTransform.js
+++ commandTransform.js
@@ -8,12 +8,26 @@ const writeFile = promisify(fs.writeFile);
 
 const readdir = promisify(fs.readdir);
 
-const loadScripts = async (readDir, writeDir) => {
+const loadScripts = async (readDir, writeDir, prefix = '') => {
   const normalizedDir = path.normalize(readDir);
 
   const files = await readdir(normalizedDir);
 
   const luaFiles = files.filter(file => path.extname(file) === '.lua');
+  const luaDirectories = files.filter(file => path.extname(file) === '');
+  const writeFilenamePath = path.normalize(writeDir);
+
+  let indexContent = '';
+  if (luaDirectories.length) {
+    for (let i = 0; i < luaDirectories.length; i++) {
+      await loadScripts(
+        path.join(normalizedDir, luaDirectories[i]),
+        path.join(writeFilenamePath, luaDirectories[i]),
+        `${prefix}-${luaDirectories[i]}`,
+      );
+      indexContent += `export * from './${luaDirectories[i]}';\n`;
+    }
+  }
 
   if (luaFiles.length === 0) {
     /**
@@ -23,24 +37,23 @@ const loadScripts = async (readDir, writeDir) => {
     throw new Error('No .lua files found!');
   }
 
-  let indexContent = '';
+  if (!fs.existsSync(writeFilenamePath)) {
+    fs.mkdirSync(writeFilenamePath);
+  }
 
   for (let i = 0; i < luaFiles.length; i++) {
     const completedFilename = path.join(normalizedDir, luaFiles[i]);
     const longName = path.basename(luaFiles[i], '.lua');
     indexContent += `export * from './${longName}';\n`;
 
-    await loadCommand(completedFilename, longName, writeDir);
+    await loadCommand(completedFilename, longName, writeFilenamePath);
   }
-
-  const writeFilenamePath = path.normalize(writeDir);
 
   await writeFile(path.join(writeFilenamePath, 'index.ts'), indexContent);
 };
 
-const loadCommand = async (filename, longName, writeDir) => {
+const loadCommand = async (filename, longName, writeFilenamePath) => {
   const filenamePath = path.resolve(filename);
-  const writeFilenamePath = path.normalize(writeDir);
 
   const content = (await readFile(filenamePath)).toString();
 
