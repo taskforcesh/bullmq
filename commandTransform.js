711710727cf043601778043794602c9ef01d76cf
rogger andré valverde flores <rogger.valverde@uni.pe>
2021-12-22 19:47:33 -0500

    chore: add deep option

--- commandTransform.js
+++ commandTransform.js
@@ -5,27 +5,44 @@ const { argv } = require('process');
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
-
 const readdir = promisify(fs.readdir);
 
-const loadScripts = async (readDir, writeDir, prefix = '') => {
+const applyCharacter = (prefix, directory, character) => {
+  return prefix ? `${prefix}${character}${directory}` : directory;
+};
+
+const loadScripts = async (
+  readDir,
+  writeDir,
+  deep = 0,
+  prefix = '',
+  pathPrefix = '',
+) => {
   const normalizedDir = path.normalize(readDir);
 
   const files = await readdir(normalizedDir);
 
   const luaFiles = files.filter(file => path.extname(file) === '.lua');
-  const luaDirectories = files.filter(file => path.extname(file) === '');
   const writeFilenamePath = path.normalize(writeDir);
 
+  if (!fs.existsSync(writeFilenamePath)) {
+    fs.mkdirSync(writeFilenamePath);
+  }
+
   let indexContent = '';
-  if (luaDirectories.length) {
-    for (let i = 0; i < luaDirectories.length; i++) {
-      await loadScripts(
-        path.join(normalizedDir, luaDirectories[i]),
-        path.join(writeFilenamePath, luaDirectories[i]),
-        `${prefix}-${luaDirectories[i]}`,
-      );
-      indexContent += `export * from './${luaDirectories[i]}';\n`;
+  if (deep) {
+    const luaDirectories = files.filter(file => path.extname(file) === '');
+    if (luaDirectories.length) {
+      for (let i = 0; i < luaDirectories.length; i++) {
+        await loadScripts(
+          path.join(normalizedDir, luaDirectories[i]),
+          path.join(writeFilenamePath, luaDirectories[i]),
+          deep,
+          applyCharacter(prefix, luaDirectories[i], '_'),
+          applyCharacter(prefix, luaDirectories[i], '/'),
+        );
+        indexContent += `export * from './${luaDirectories[i]}';\n`;
+      }
     }
   }
 
@@ -37,22 +54,30 @@ const loadScripts = async (readDir, writeDir, prefix = '') => {
     throw new Error('No .lua files found!');
   }
 
-  if (!fs.existsSync(writeFilenamePath)) {
-    fs.mkdirSync(writeFilenamePath);
-  }
-
   for (let i = 0; i < luaFiles.length; i++) {
     const completedFilename = path.join(normalizedDir, luaFiles[i]);
     const longName = path.basename(luaFiles[i], '.lua');
     indexContent += `export * from './${longName}';\n`;
 
-    await loadCommand(completedFilename, longName, writeFilenamePath);
+    await loadCommand(
+      completedFilename,
+      longName,
+      writeFilenamePath,
+      prefix,
+      pathPrefix,
+    );
   }
 
   await writeFile(path.join(writeFilenamePath, 'index.ts'), indexContent);
 };
 
-const loadCommand = async (filename, longName, writeFilenamePath) => {
+const loadCommand = async (
+  filename,
+  longName,
+  writeFilenamePath,
+  prefix,
+  pathPrefix,
+) => {
   const filenamePath = path.resolve(filename);
 
   const content = (await readFile(filenamePath)).toString();
@@ -62,7 +87,8 @@ const loadCommand = async (filename, longName, writeFilenamePath) => {
 
   const newContent = `const content = \`${content}\`;
 
-export const ${name} = {
+export const ${prefix ? `${prefix}_${name}` : name} = {
+  ref: '${pathPrefix ? `${pathPrefix}/${name}` : name}',
   content,${
     numberOfKeys
       ? `
@@ -74,4 +100,4 @@ export const ${name} = {
   await writeFile(path.join(writeFilenamePath, longName + '.ts'), newContent);
 };
 
-loadScripts(argv[2], argv[3]);
+loadScripts(argv[2], argv[3], argv[4], argv[5], argv[6]);
