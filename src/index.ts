ad4d88336a9fdab06836685ca09f890a13232088
rogger andré valverde flores <rogger.valverde@uni.pe>
2022-01-19 20:58:50 -0500

    feat(queue): add JobTypes for better typing

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './classes';
 export * from './commands';
 export * from './enums';
 export * from './interfaces';
+export * from './types';
 export * from './utils';
---
+++ src/types/job-types.ts
@@ -0,0 +1,10 @@
+export type JobTypes =
+  | 'completed'
+  | 'failed'
+  | 'delayed'
+  | 'repeat'
+  | 'waiting-children'
+  | 'active'
+  | 'waiting'
+  | 'wait'
+  | 'paused';
