f8619a56ee445a3f2c2b00e63b71c173d6597638
rogger andré valverde flores <rogger.valverde@uni.pe>
2022-01-28 19:50:55 -0500

    refactor: pass batches to includes

--- src/commands/moveStalledJobsToWait-8.lua
+++ src/commands/moveStalledJobsToWait-8.lua
@@ -23,18 +23,8 @@
 ]]
 local rcall = redis.call
 
-local function batches(n, batchSize)
-  local i = 0
-
-  return function()
-    local from = i * batchSize + 1
-    i = i + 1
-    if (from <= n) then
-      local to = math.min(from + batchSize - 1, n)
-      return from, to
-    end
-  end
-end
+-- Includes
+--- @include "includes/batches"
 
 -- Check if we need to check for stalled jobs now.
 if rcall("EXISTS", KEYS[5]) == 1 then return {{}, {}} end
