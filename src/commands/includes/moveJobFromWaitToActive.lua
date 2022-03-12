0b70f96074329c48ec4df92c77d47a007cd1062a
rogger andré valverde flores <rogger.valverde@uni.pe>
2022-03-11 21:06:20 -0500

    docs: add input description

--- src/commands/includes/moveJobFromWaitToActive.lua
+++ src/commands/includes/moveJobFromWaitToActive.lua
@@ -1,6 +1,23 @@
 
 --[[
   Function to move job from wait state to active.
+  Input:
+    keys[1] wait key
+    keys[2] active key
+    keys[3] priority key
+    keys[4] stream events key
+    keys[5] stalled key
+
+    -- Rate limiting
+    keys[6] rate limiter key
+    keys[7] delayed key
+
+    -- Delay events
+    keys[8] delay stream key
+
+    opts - token - lock token
+    opts - lockDuration
+    opts - limiter
 ]]
 
 local function moveJobFromWaitToActive(keys, keyPrefix, jobId, processedOn, opts)
