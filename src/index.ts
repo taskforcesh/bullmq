382338c0db6300b7276ac4c711ccc559739640b4
Everspace <7560635+Everspace@users.noreply.github.com>
2022-03-13 12:25:33 -0700

    feat(./src/index.ts): add worker and job infering types
    Add the QueueWorker<T> and QueueJob<T> types to allow one to infer the types on a corrisponding
    worker easily.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,23 @@ export * from './enums';
 export * from './interfaces';
 export * from './types';
 export * from './utils';
+
+import type { Queue } from './classes/queue';
+import type { Worker } from './classes/worker';
+import type { Job } from './classes/job';
+
+export type QueueWorker<Q> = Q extends Queue<
+  infer DataType,
+  infer ResultType,
+  infer NameType
+>
+  ? Worker<DataType, ResultType, NameType>
+  : unknown;
+
+export type QueueJob<Q> = Q extends Queue<
+  infer DataType,
+  infer ResultType,
+  infer NameType
+>
+  ? Job<DataType, ResultType, NameType>
+  : unknown;
