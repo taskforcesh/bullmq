9a61300d19c4f17a97880d044282d89ae0ffa6d1
fractalawareness <fractalawareness@gmail.com>
2022-06-01 04:21:30 +0300

    Check finishedOn on failed event in worker test

--- tests/test_worker.ts
+++ tests/test_worker.ts
@@ -1012,6 +1012,7 @@ describe('workers', function () {
     await new Promise<void>(resolve => {
       worker.once('failed', async (job, err) => {
         expect(job).to.be.ok;
+        expect(job.finishedOn).to.be.string;
         expect(job.data.foo).to.be.eql('bar');
         expect(err).to.be.eql(jobError);
         resolve();
