75d527581fa1ebbffb965288e1ff0e5342610b72
rogger andré valverde flores <rogger.valverde@uni.pe>
2022-09-23 19:49:04 -0500

    ci: add transform:command in build script

--- src/classes/redis-connection.ts
+++ src/classes/redis-connection.ts
@@ -268,9 +268,9 @@ export class RedisConnection extends EventEmitter {
       if (lines[i].indexOf(maxMemoryPolicyPrefix) === 0) {
         const maxMemoryPolicy = lines[i].substr(maxMemoryPolicyPrefix.length);
         if (maxMemoryPolicy !== 'noeviction') {
-          /* throw new Error(
+          throw new Error(
             `Eviction policy is ${maxMemoryPolicy}. It should be "noeviction"`,
-          );*/
+          );
         }
       }
 
