bda3a0d1a1d0c059bf0a1c22377c5e940a460eb7
rogger andré valverde flores <rogger.valverde@uni.pe>
2022-11-28 21:01:02 -0500

    ci: harden release and test permissions

--- src/commands/includes/isLocked.lua
+++ src/commands/includes/isLocked.lua
@@ -13,19 +13,19 @@ local function isLocked( prefix, jobId)
   local lockKey = jobKey .. ':lock'
   local lock = rcall("GET", lockKey)
   if not lock then
-      local dependencies = rcall("SMEMBERS", jobKey .. ":dependencies")
-      if (#dependencies > 0) then
-          for i, childJobKey in ipairs(dependencies) do
-              -- We need to get the jobId for this job.
-              local childJobId = getJobIdFromKey(childJobKey)
-              local childJobPrefix = getJobKeyPrefix(childJobKey, childJobId)
-              local result = isLocked( childJobPrefix, childJobId )
-              if result then
-                  return true
-              end
-          end
+    local dependencies = rcall("SMEMBERS", jobKey .. ":dependencies")
+    if (#dependencies > 0) then
+      for i, childJobKey in ipairs(dependencies) do
+        -- We need to get the jobId for this job.
+        local childJobId = getJobIdFromKey(childJobKey)
+        local childJobPrefix = getJobKeyPrefix(childJobKey, childJobId)
+        local result = isLocked( childJobPrefix, childJobId )
+        if result then
+          return true
+        end
       end
-      return false
+    end
+    return false
   end
   return true
 end
