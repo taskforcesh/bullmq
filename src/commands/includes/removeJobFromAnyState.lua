f7197a269add32b0ed2b430e45072ce20acd08c0
Niek van der Maas <mail@niekvandermaas.nl>
2022-12-27 10:30:53 +0100

    Fix nil check

--- src/commands/includes/removeJobFromAnyState.lua
+++ src/commands/includes/removeJobFromAnyState.lua
@@ -7,16 +7,16 @@
 
 local function removeJobFromAnyState( prefix, jobId)
   -- We start with the ZSCORE checks, since they have O(1) complexity
-  if rcall("ZSCORE", prefix .. "completed", jobId) ~= nil then
+  if rcall("ZSCORE", prefix .. "completed", jobId) then
     rcall("ZREM", prefix .. "completed", jobId)
     return "completed"
-  elseif rcall("ZSCORE", prefix .. "waiting-children", jobId) ~= nil then
+  elseif rcall("ZSCORE", prefix .. "waiting-children", jobId) then
     rcall("ZREM", prefix .. "waiting-children", jobId)
     return "waiting-children"
-  elseif rcall("ZSCORE", prefix .. "delayed", jobId) ~= nil then
+  elseif rcall("ZSCORE", prefix .. "delayed", jobId) then
     rcall("ZREM", prefix .. "delayed", jobId)
     return "delayed"
-  elseif rcall("ZSCORE", prefix .. "failed", jobId) ~= nil then
+  elseif rcall("ZSCORE", prefix .. "failed", jobId) then
     rcall("ZREM", prefix .. "failed", jobId)
     return "failed"
   -- We remove only 1 element from the list, since we assume they are not added multiple times
