6357128bcb4636913ee81fde735e4ff4f91fef76
Manuel Astudillo <manuel@optimalbits.com>
2023-02-23 23:21:18 +0100

    docs(worker): improve options documentation

--- src/interfaces/worker-options.ts
+++ src/interfaces/worker-options.ts
@@ -15,12 +15,15 @@ export type Processor<T = any, R = any, N extends string = string> = (
 export interface WorkerOptions extends QueueBaseOptions {
   /**
    * Condition to start processor at instance creation.
+   *
+   * @default true
    */
   autorun?: boolean;
   /**
    * Amount of jobs that a single worker is allowed to work on
    * in parallel.
    *
+   * @default 1
    * @see {@link https://docs.bullmq.io/guide/workers/concurrency}
    */
   concurrency?: number;
@@ -40,11 +43,15 @@ export interface WorkerOptions extends QueueBaseOptions {
    * Amount of times a job can be recovered from a stalled state
    * to the `wait` state. If this is exceeded, the job is moved
    * to `failed`.
+   *
+   * @default 1
    */
   maxStalledCount?: number;
 
   /**
    * Number of milliseconds between stallness checks.
+   *
+   * @default 30000
    */
   stalledInterval?: number;
 
@@ -52,14 +59,48 @@ export interface WorkerOptions extends QueueBaseOptions {
    *  Skip stalled check for this worker. Note that other workers could still
    *  perform stalled checkd and move jobs back to wait for jobs being processed
    *  by this worker.
+   *
+   *  @default false
    */
   skipStalledCheck?: boolean;
 
-  skipDelayCheck?: boolean;
+  /**
+   *
+   * Number of seconds to long poll for jobs when the queue is empty.
+   *
+   * @default 5
+   */
   drainDelay?: number;
+
+  /**
+   *
+   * Duration of the lock for the job in milliseconds. The lock represents that
+   * a worker is processing the job. If the lock is lost, the job will be eventually
+   * be picked up by the stalled checker and move back to wait so that another worker
+   * can process it again.
+   *
+   * @default 30000
+   */
   lockDuration?: number;
+
+  /**
+   * The time in milliseconds before the lock is automatically renewed.
+   *
+   * It is not recommended to modify this value, which is by default set to
+   * halv the lockDuration value, which is optimal for most use cases.
+   */
   lockRenewTime?: number;
+
+  /**
+   * This is an internal option that should not be modified.
+   *
+   * @default 15000
+   */
   runRetryDelay?: number;
+
+  /**
+   * More advanced options.
+   */
   settings?: AdvancedOptions; // FIXME only backoffStrategies is used
 }
 
