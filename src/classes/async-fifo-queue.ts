38e0708d12e52373b9882707b2676ad7f06070a4
Manuel Astudillo <manuel@optimalbits.com>
2023-03-29 10:14:42 +0200

    chore(asyncfifoqueue): add TODO comment

--- src/classes/async-fifo-queue.ts
+++ src/classes/async-fifo-queue.ts
@@ -4,6 +4,11 @@
  * A minimal FIFO queue for asyncrhonous operations. Allows adding asynchronous operations
  * and consume them in the order they are resolved.
  *
+ *  TODO: Optimize using a linked list for the queue instead of an array.
+ *  Current implementation requires memory copies when shifting the queue.
+ *  For a linked linked implementation, we can exploit the fact that the
+ *  maximum number of elements in the list will never exceen the concurrency factor
+ *  of the worker, so the nodes of the list could be pre-allocated.
  */
 
 export class AsyncFifoQueue<T> {
