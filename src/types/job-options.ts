c22ab7b2a1dba0f1ed2636e0b0d104bf93f3422e
roggervalf <rogger.valverde@uni.pe>
2023-05-05 19:52:06 -0500

    chore: add missing kl attribute in interface

--- src/types/job-options.ts
+++ src/types/job-options.ts
@@ -15,4 +15,9 @@ export type RedisJobOptions = BaseJobOptions & {
    * If true, moves parent to failed.
    */
   fpof?: boolean;
+
+  /**
+   * Maximum amount of log entries that will be preserved
+   */
+  kl?: number;
 };
