ca17364cc2a52f6577fb66f09ec3168bbf9f1e07
ElFisherMan <36671468+ElFisherMan@users.noreply.github.com>
2023-07-03 11:45:14 +0300

    feat(common): add option to change repeatable jobs redis key hash algorithm (#2023)

--- src/interfaces/advanced-options.ts
+++ src/interfaces/advanced-options.ts
@@ -5,6 +5,12 @@ export interface AdvancedRepeatOptions {
    * A custom cron strategy.
    */
   repeatStrategy?: RepeatStrategy;
+
+  /**
+   * A hash algorithm to be used when trying to create the job redis key.
+   * Default - md5
+   */
+  repeatKeyHashAlgorithm?: string;
 }
 
 export interface AdvancedOptions extends AdvancedRepeatOptions {
