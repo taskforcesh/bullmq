48ec43fe9f0df026cde12de3299aad3c54378522
roggervalf <rogger.valverde@uni.pe>
2023-10-04 22:02:49 -0500

    fix(sandbox): update progress value on job instance

--- src/interfaces/sandboxed-job.ts
+++ src/interfaces/sandboxed-job.ts
@@ -5,7 +5,7 @@ import { JobJson } from './job-json';
  * @see {@link https://docs.bullmq.io/guide/workers/sandboxed-processors}
  */
 export interface SandboxedJob<T = any, R = any>
-  extends Omit<JobJson, 'data' | 'opts' | 'progress' | 'returnValue'> {
+  extends Omit<JobJson, 'data' | 'opts' | 'returnValue'> {
   data: T;
   opts: JobsOptions;
   moveToDelayed: (timestamp: number, token?: string) => Promise<void>;
