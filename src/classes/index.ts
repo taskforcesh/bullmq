0379dd942329dc1cae0b76ea01491a4fb8cceb81
Rogger Valverde <rogger.valverde@uni.pe>
2023-12-03 20:59:53 -0600

    refactor(erros): convert rate limit error into a new class definition (#2311)

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -2,7 +2,7 @@ export * from './async-fifo-queue';
 export * from './backoffs';
 export * from './child-pool';
 export * from './child-processor';
-export * from './delayed-error';
+export * from './errors';
 export * from './flow-producer';
 export * from './job';
 // export * from './main'; this file must not be exported
@@ -16,6 +16,4 @@ export * from './redis-connection';
 export * from './repeat';
 export * from './sandbox';
 export * from './scripts';
-export * from './unrecoverable-error';
-export * from './waiting-children-error';
 export * from './worker';
