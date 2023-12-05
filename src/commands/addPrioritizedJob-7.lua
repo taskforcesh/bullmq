2cd09ed63d8eda84cf0a557ced382c7415db1bb8
roggervalf <rogger.valverde@uni.pe>
2023-12-04 21:05:12 -0700

    chore: remove few todos regarding parent jobs

--- src/commands/addPrioritizedJob-7.lua
+++ src/commands/addPrioritizedJob-7.lua
@@ -103,8 +103,6 @@ rcall("XADD", eventsKey, "MAXLEN", "~", maxEvents, "*", "event", "waiting",
       "jobId", jobId)
 
 -- Check if this job is a child of another job, if so add it to the parents dependencies
--- TODO: Should not be possible to add a child job to a parent that is not in the "waiting-children" status.
--- fail in this case.
 if parentDependenciesKey ~= nil then
     rcall("SADD", parentDependenciesKey, jobIdKey)
 end
