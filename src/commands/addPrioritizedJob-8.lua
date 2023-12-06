272ec69557f601a138e1aaba739f7e7878d5344b
Manuel Astudillo <manuel@optimalbits.com>
2023-12-06 12:27:13 +0100

    fix(flows): add meta key to queues created with flows

--- src/commands/addPrioritizedJob-8.lua
+++ src/commands/addPrioritizedJob-8.lua
@@ -62,6 +62,7 @@ local parentData
 --- @include "includes/addJobWithPriority"
 --- @include "includes/getTargetQueueList"
 --- @include "includes/updateExistingJobsParent"
+--- @include "includes/getOrSetMaxEvents"
 
 if parentKey ~= nil then
     if rcall("EXISTS", parentKey) ~= 1 then return -5 end
@@ -71,7 +72,7 @@ end
 
 local jobCounter = rcall("INCR", idKey)
 
-local maxEvents = rcall("HGET", metaKey, "opts.maxLenEvents") or 10000
+local maxEvents = getOrSetMaxEvents(metaKey)
 
 local parentDependenciesKey = args[7]
 local timestamp = args[4]
