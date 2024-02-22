a4630408d3f22c09952ecf539d93085c90c78429
roggervalf <rogger.valverde@uni.pe>
2024-02-21 20:54:09 -0500

    fix(add-job): fix parent job cannot be replaced error message

--- src/classes/scripts.ts
+++ src/classes/scripts.ts
@@ -447,7 +447,7 @@ export class Scripts {
         );
       case ErrorCode.ParentJobCannotBeReplaced:
         return new Error(
-          `The parent job of job ${jobId} cannot be replaced. ${command}`,
+          `The parent job ${jobId} cannot be replaced. ${command}`,
         );
       default:
         return new Error(`Unknown code ${code} error for ${jobId}. ${command}`);
