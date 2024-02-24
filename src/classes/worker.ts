ac4f874b8468a6d7852b87e7e7e85a9e85c9d9f1
Igor Savin <iselwin@gmail.com>
2024-02-24 15:51:42 +0200

    feat(worker): add support for conditional connection closure

--- src/classes/worker.ts
+++ src/classes/worker.ts
@@ -807,7 +807,7 @@ export class Worker<
    *
    * @returns Promise that resolves when the worker has been closed.
    */
-  close(force = false): Promise<void> {
+  close(force = false, closeConnection = true): Promise<void> {
     if (this.closing) {
       return this.closing;
     }
@@ -841,8 +841,8 @@ export class Worker<
         })
         .finally(() => clearTimeout(this.extendLocksTimer))
         .finally(() => clearTimeout(this.stalledCheckTimer))
-        .finally(() => client && client.disconnect())
-        .finally(() => this.connection.close())
+        .finally(() => closeConnection && client && client.disconnect())
+        .finally(() => closeConnection && this.connection.close())
         .finally(() => this.emit('closed'));
       this.closed = true;
     })();
