5a3408e4b89268633dbd7905a41237b7f8ead53e
roggervalf <rogger.valverde@uni.pe>
2024-03-04 22:20:59 -0500

    refactor: add toString util method

--- src/utils.ts
+++ src/utils.ts
@@ -222,4 +222,28 @@ export const errorToJSON = (value: any): Record<string, any> => {
   return error;
 };
 
-export const QUEUE_EVENT_SUFFIX =':qe';
+const INFINITY = 1 / 0;
+
+export const toString = (value: any): string => {
+  if (value == null) {
+    return '';
+  }
+  // Exit early for strings to avoid a performance hit in some environments.
+  if (typeof value === 'string') {
+    return value;
+  }
+  if (Array.isArray(value)) {
+    // Recursively convert values (susceptible to call stack limits).
+    return `${value.map(other => (other == null ? other : toString(other)))}`;
+  }
+  if (
+    typeof value == 'symbol' ||
+    Object.prototype.toString.call(value) == '[object Symbol]'
+  ) {
+    return value.toString();
+  }
+  const result = `${value}`;
+  return result === '0' && 1 / value === -INFINITY ? '-0' : result;
+};
+
+export const QUEUE_EVENT_SUFFIX = ':qe';
