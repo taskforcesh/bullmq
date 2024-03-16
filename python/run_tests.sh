34946c4b29cc9e7d5ae81f8fd170a2e539ac6279
Rogger Valverde <rogger.valverde@uni.pe>
2024-03-15 21:48:10 -0600

    feat(job): add log method [python] (#2476) ref #2472

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 redis-cli flushall
-python3 -m unittest -v tests.bulk_tests
-python3 -m unittest -v tests.delay_tests
-python3 -m unittest -v tests.flow_tests
 python3 -m unittest -v tests.job_tests
-python3 -m unittest -v tests.queue_tests
-python3 -m unittest -v tests.worker_tests
