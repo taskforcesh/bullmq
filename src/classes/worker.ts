d6066f416494b938e972de90be86afad92322def
Rogger Valverde <rogger.valverde@uni.pe>
2024-04-21 12:44:30 -0600

    test(worker): improve getBlockTimeout tests (#2535)

--- src/classes/worker.ts
+++ src/classes/worker.ts
@@ -38,10 +38,6 @@ import {
 
 // 10 seconds is the maximum time a BRPOPLPUSH can block.
 const maximumBlockTimeout = 10;
-/* 1 millisecond is chosen because the granularity of our timestamps are milliseconds.
-Obviously we can still process much faster than 1 job per millisecond but delays and rate limits
-will never work with more accuracy than 1ms. */
-const minimumBlockTimeout = 0.001;
 
 // note: sandboxed processors would also like to define concurrency per process
 // for better resource utilization.
@@ -594,7 +590,10 @@ export class Worker<
 
   get minimumBlockTimeout(): number {
     return this.blockingConnection.capabilities.canBlockFor1Ms
-      ? minimumBlockTimeout
+      ? /* 1 millisecond is chosen because the granularity of our timestamps are milliseconds.
+Obviously we can still process much faster than 1 job per millisecond but delays and rate limits
+will never work with more accuracy than 1ms. */
+        0.001
       : 0.002;
   }
 
@@ -657,19 +656,16 @@ export class Worker<
 
     // when there are delayed jobs
     if (blockUntil) {
-      let blockTimeout;
       const blockDelay = blockUntil - Date.now();
       // when we reach the time to get new jobs
       if (blockDelay < this.minimumBlockTimeout * 1000) {
         return this.minimumBlockTimeout;
       } else {
-        blockTimeout = blockDelay / 1000;
+        // We restrict the maximum block timeout to 10 second to avoid
+        // blocking the connection for too long in the case of reconnections
+        // reference: https://github.com/taskforcesh/bullmq/issues/1658
+        return Math.min(blockDelay / 1000, maximumBlockTimeout);
       }
-
-      // We restrict the maximum block timeout to 10 second to avoid
-      // blocking the connection for too long in the case of reconnections
-      // reference: https://github.com/taskforcesh/bullmq/issues/1658
-      return Math.min(blockTimeout, maximumBlockTimeout);
     } else {
       return Math.max(opts.drainDelay, this.minimumBlockTimeout);
     }
