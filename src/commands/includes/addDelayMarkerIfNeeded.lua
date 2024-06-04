84d72e57d409c319a4c748afef5678d4c01b686f
roggervalf <rogger.valverde@uni.pe>
2024-06-03 22:41:50 -0500

    perf(job): set processedBy using hmset

--- src/commands/includes/addDelayMarkerIfNeeded.lua
+++ src/commands/includes/addDelayMarkerIfNeeded.lua
@@ -6,10 +6,10 @@
 --- @include "getNextDelayedTimestamp"
 
 local function addDelayMarkerIfNeeded(markerKey, delayedKey)
-    local nextTimestamp = getNextDelayedTimestamp(delayedKey)
-    if nextTimestamp ~= nil then
-        -- Replace the score of the marker with the newest known
-        -- next timestamp.
-        rcall("ZADD", markerKey, nextTimestamp, "1")
-    end
+  local nextTimestamp = getNextDelayedTimestamp(delayedKey)
+  if nextTimestamp ~= nil then
+    -- Replace the score of the marker with the newest known
+    -- next timestamp.
+    rcall("ZADD", markerKey, nextTimestamp, "1")
+  end
 end
