88b8f6c49c9642a252fbb6ef3b2b2e4260ebed8c
semantic-release <semantic-release>
2024-06-04 23:48:19 +0000

    2.7.7
    Automatically generated by python-semantic-release

--- python/bullmq/__init__.py
+++ python/bullmq/__init__.py
@@ -3,7 +3,7 @@ BullMQ
 
 A background job processor and message queue for Python based on Redis.
 """
-__version__ = "2.7.6"
+__version__ = "2.7.7"
 __author__ = 'Taskforce.sh Inc.'
 __credits__ = 'Taskforce.sh Inc.'
 
