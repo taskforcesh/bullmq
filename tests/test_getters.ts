6e992504b2a7a2fa76f1d04ad53d1512e98add7f
Rogger Valverde <rogger.valverde@uni.pe>
2024-06-15 00:23:01 -0600

    fix(priority): consider paused state when calling getCountsPerPriority (python) (#2609)

--- tests/test_getters.ts
+++ tests/test_getters.ts
@@ -866,6 +866,31 @@ describe('Jobs getters', function () {
         '3': 10,
       });
     });
+
+    describe('when queue is paused', () => {
+      it('returns job counts per priority', async () => {
+        await queue.waitUntilReady();
+
+        await queue.pause();
+        const jobs = Array.from(Array(42).keys()).map(index => ({
+          name: 'test',
+          data: {},
+          opts: {
+            priority: index % 4,
+          },
+        }));
+        await queue.addBulk(jobs);
+
+        const counts = await queue.getCountsPerPriority([0, 1, 2, 3]);
+
+        expect(counts).to.be.eql({
+          '0': 11,
+          '1': 11,
+          '2': 10,
+          '3': 10,
+        });
+      });
+    });
   });
 
   describe('.getDependencies', () => {
