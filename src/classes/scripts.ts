2c913b2a82311fea6ce6c03c01eed5e58035b572
roggervalf <rogger.valverde@uni.pe>
2024-09-19 20:52:56 -0500

    refactor(job): pass prefix into changePriority for extension

--- src/classes/scripts.ts
+++ src/classes/scripts.ts
@@ -846,7 +846,7 @@ export class Scripts {
     }
   }
 
-  private changePriorityArgs(
+  protected changePriorityArgs(
     jobId: string,
     priority = 0,
     lifo = false,
@@ -863,7 +863,7 @@ export class Scripts {
 
     return keys.concat([
       priority,
-      this.queue.toKey(jobId),
+      this.queue.toKey(''),
       jobId,
       lifo ? 1 : 0,
     ]);
