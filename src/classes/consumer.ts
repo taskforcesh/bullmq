4d9454176d0f6e61b925ec1d5cf35fc264611bb3
org <org@everc.com>
2024-10-21 21:38:46 +0300

    feat: adding streaming components

--- src/classes/consumer.ts
+++ src/classes/consumer.ts
@@ -1,6 +1,7 @@
 import { RedisConnection } from '@src/classes/redis-connection';
 import { QueueBase } from '@src/classes/queue-base';
 import { ProducerOptions } from '@src/interfaces/producer-options';
+import { v4 } from 'uuid';
 
 export class Consumer<DataType = any> extends QueueBase {
   constructor(
@@ -27,13 +28,53 @@ export class Consumer<DataType = any> extends QueueBase {
       });
   }
 
-  async consume(cb: (data: DataType) => Promise<void>): Promise<void> {
-    const client = await this.client;
-    const stream = client.xread('BLOCK', 0, 'STREAMS', this.name, '>');
-    stream.on('data', async data => {
-      const [key, [id, data]] = data;
-      await cb(JSON.parse(data));
-      await client.xack(this.name, this.name, id);
-    });
+  consume(consumerGroup: string, cb: (data: DataType) => Promise<void>): void {
+    this.waitUntilReady()
+      .then(async () => {
+        const streamName = `${this.name}`;
+        const consumerName = v4();
+
+        const client = await this.client;
+        await client.xgroup(
+          'CREATE',
+          streamName,
+          consumerGroup,
+          '0',
+          'MKSTREAM',
+        );
+
+        while (!this.closing) {
+          const result = (await client.xreadgroup(
+            'GROUP',
+            consumerGroup,
+            consumerName,
+            'COUNT',
+            this.opts.pubsub.batchSize || 1,
+            'BLOCK',
+            this.opts.pubsub.blockTime || 1000,
+            'STREAMS',
+            streamName,
+            '>',
+          )) as XReadGroupResult[] | null;
+
+          if (result && result.length > 0) {
+            const [, messages] = result[0];
+            for (const [id, fields] of messages) {
+              const jobData: Record<string, string> = {};
+              for (let i = 0; i < fields.length; i += 2) {
+                const key = fields[i];
+                jobData[key] = fields[i + 1];
+              }
+              await queue.add(
+                'default',
+                JSON.parse(jobData['data']),
+                JSON.parse(jobData['opts']),
+              );
+              await client.xack(streamName, consumerGroup, id);
+            }
+          }
+        }
+      })
+      .catch(error => this.emit('error', error));
   }
 }
