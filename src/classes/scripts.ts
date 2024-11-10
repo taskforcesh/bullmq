937b8f85eb229a1577ff934e2c0efa1b19ad80b0
Manuel Astudillo <manuel@optimalbits.com>
2024-11-10 21:10:26 +0100

    fix(job-scheculer): avoid hazards when upserting job schedulers concurrently

--- src/classes/scripts.ts
+++ src/classes/scripts.ts
@@ -308,12 +308,12 @@ export class Scripts {
   }
 
   async addJobScheduler(
+    client: RedisClient,
     jobSchedulerId: string,
     nextMillis: number,
     opts: RepeatableOptions,
   ): Promise<string> {
     const queueKeys = this.queue.keys;
-    const client = await this.queue.client;
 
     const keys: (string | number | Buffer)[] = [
       queueKeys.repeat,
@@ -339,11 +339,10 @@ export class Scripts {
   }
 
   async updateJobSchedulerNextMillis(
+    client: RedisClient,
     jobSchedulerId: string,
     nextMillis: number,
   ): Promise<number> {
-    const client = await this.queue.client;
-
     return client.zadd(this.queue.keys.repeat, nextMillis, jobSchedulerId);
   }
 
