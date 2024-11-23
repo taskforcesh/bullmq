ea157a8ac52f9569deffc720d65d885bbb9b31a5
roggervalf <rogger.valverde@uni.pe>
2024-11-22 23:54:01 -0500

    fix(queue): remove repeat instance in favor of job-scheduler

--- src/interfaces/repeat-options.ts
+++ src/interfaces/repeat-options.ts
@@ -12,14 +12,6 @@ export interface RepeatOptions extends Omit<ParserOptions, 'iterator'> {
   pattern?: string;
 
   /**
-   * Custom repeatable key. This is the key that holds the "metadata"
-   * of a given repeatable job. This key is normally auto-generated but
-   * it is sometimes useful to specify a custom key for easier retrieval
-   * of repeatable jobs.
-   */
-  key?: string;
-
-  /**
    * Number of times the job should repeat at max.
    */
   limit?: number;
@@ -47,17 +39,4 @@ export interface RepeatOptions extends Omit<ParserOptions, 'iterator'> {
    * Internal property to store the previous time the job was executed.
    */
   prevMillis?: number;
-
-  /**
-   * Internal property to store the offset to apply to the next iteration.
-   *
-   * @deprecated
-   */
-  offset?: number;
-
-  /**
-   * Internal property to store de job id
-   * @deprecated
-   */
-  jobId?: string;
 }
