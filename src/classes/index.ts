97e3a65b8515aa18e5bc3490c36c08e9de64552a
roggervalf <rogger.valverde@uni.pe>
2024-11-23 00:01:22 -0500

    chore: remove repeat from exports

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -15,7 +15,6 @@ export * from './queue-getters';
 export * from './queue-keys';
 export * from './queue';
 export * from './redis-connection';
-export * from './repeat';
 export * from './sandbox';
 export * from './scripts';
 export * from './worker';
