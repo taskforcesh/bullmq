7fc51e2e33468854c3beb460077489cec5ce64dc
roggervalf <rogger.valverde@uni.pe>
2024-11-23 14:33:28 -0500

    chore: remove debug statements

--- src/commands/getJobSchedulerTemplate-2.lua
+++ src/commands/getJobSchedulerTemplate-2.lua
@@ -11,14 +11,8 @@ local rcall = redis.call
 
 local millis = rcall("ZSCORE", KEYS[1], ARGV[1])
 
-rcall('SET', 'DEBUG1', 'here')
-
 if millis ~= false then
-  rcall('SET', 'DEBUG', millis)
   local templateJobId = "repeat:" .. ARGV[1] .. ":" .. millis
-
-    rcall('SET', 'DEBUG2', templateJobId)
-    rcall('SET', 'DEBUG3', KEYS[2] .. templateJobId)
 
   return {rcall("HGETALL", KEYS[2] .. templateJobId), templateJobId} -- get job data
 end
