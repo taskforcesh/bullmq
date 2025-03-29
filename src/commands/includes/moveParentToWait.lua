09f6ef7558b9b4ea7e0029f8e3aeeb5960255d7e
Manuel Astudillo <manuel@optimalbits.com>
2025-03-29 16:59:42 +0100

    chore(flows): only move parent to wait if status waiting-children

--- src/commands/includes/moveParentToWait.lua
+++ src/commands/includes/moveParentToWait.lua
@@ -7,40 +7,44 @@
 --- @include "isQueuePausedOrMaxed"
 --- @include "getTargetQueueList"
 local function moveParentToWait(parentQueueKey, parentKey, parentId, timestamp)
-    rcall("ZREM", parentQueueKey .. ":waiting-children", parentId)
-    local parentWaitKey = parentQueueKey .. ":wait"
-    local parentPausedKey = parentQueueKey .. ":paused"
-    local parentActiveKey = parentQueueKey .. ":active"
-    local parentMetaKey = parentQueueKey .. ":meta"
+    local isParentWaitingChildren = rcall("ZSCORE", parentQueueKey .. ":waiting-children", parentId)
+    if isParentWaitingChildren then
+        rcall("ZREM", parentQueueKey .. ":waiting-children", parentId)
+        local parentWaitKey = parentQueueKey .. ":wait"
+        local parentPausedKey = parentQueueKey .. ":paused"
+        local parentActiveKey = parentQueueKey .. ":active"
+        local parentMetaKey = parentQueueKey .. ":meta"
 
-    local parentMarkerKey = parentQueueKey .. ":marker"
-    local jobAttributes = rcall("HMGET", parentKey, "priority", "delay")
-    local priority = tonumber(jobAttributes[1]) or 0
-    local delay = tonumber(jobAttributes[2]) or 0
+        local parentMarkerKey = parentQueueKey .. ":marker"
+        local jobAttributes = rcall("HMGET", parentKey, "priority", "delay")
+        local priority = tonumber(jobAttributes[1]) or 0
+        local delay = tonumber(jobAttributes[2]) or 0
 
-    -- ignore dependencies if any left
-    rcall("HSET", parentKey, "igdp", 1)
+        -- ignore dependencies if any left
+        rcall("HSET", parentKey, "igdp", 1)
 
-    if delay > 0 then
-        local delayedTimestamp = tonumber(timestamp) + delay
-        local score = delayedTimestamp * 0x1000
-        local parentDelayedKey = parentQueueKey .. ":delayed"
-        rcall("ZADD", parentDelayedKey, score, parentId)
-        rcall("XADD", parentQueueKey .. ":events", "*", "event", "delayed", "jobId", parentId, "delay", delayedTimestamp)
+        if delay > 0 then
+            local delayedTimestamp = tonumber(timestamp) + delay
+            local score = delayedTimestamp * 0x1000
+            local parentDelayedKey = parentQueueKey .. ":delayed"
+            rcall("ZADD", parentDelayedKey, score, parentId)
+            rcall("XADD", parentQueueKey .. ":events", "*", "event", "delayed", "jobId", parentId, "delay",
+                delayedTimestamp)
 
-        addDelayMarkerIfNeeded(parentMarkerKey, parentDelayedKey)
-    else
-        if priority == 0 then
-            local parentTarget, isParentPausedOrMaxed = getTargetQueueList(parentMetaKey, parentActiveKey,
-                parentWaitKey, parentPausedKey)
-            addJobInTargetList(parentTarget, parentMarkerKey, "RPUSH", isParentPausedOrMaxed, parentId)
+            addDelayMarkerIfNeeded(parentMarkerKey, parentDelayedKey)
         else
-            local isPausedOrMaxed = isQueuePausedOrMaxed(parentMetaKey, parentActiveKey)
-            addJobWithPriority(parentMarkerKey, parentQueueKey .. ":prioritized", priority, parentId,
-                parentQueueKey .. ":pc", isPausedOrMaxed)
-        end
+            if priority == 0 then
+                local parentTarget, isParentPausedOrMaxed = getTargetQueueList(parentMetaKey, parentActiveKey,
+                    parentWaitKey, parentPausedKey)
+                addJobInTargetList(parentTarget, parentMarkerKey, "RPUSH", isParentPausedOrMaxed, parentId)
+            else
+                local isPausedOrMaxed = isQueuePausedOrMaxed(parentMetaKey, parentActiveKey)
+                addJobWithPriority(parentMarkerKey, parentQueueKey .. ":prioritized", priority, parentId,
+                    parentQueueKey .. ":pc", isPausedOrMaxed)
+            end
 
-        rcall("XADD", parentQueueKey .. ":events", "*", "event", "waiting", "jobId", parentId, "prev",
-            "waiting-children")
+            rcall("XADD", parentQueueKey .. ":events", "*", "event", "waiting", "jobId", parentId, "prev",
+                "waiting-children")
+        end
     end
 end
--- src/commands/includes/moveParentToWaitIfNeeded.lua
+++ src/commands/includes/moveParentToWaitIfNeeded.lua
@@ -3,8 +3,8 @@
 ]]
 --- @include "moveParentToWait"
 local function moveParentToWaitIfNeeded(parentQueueKey, parentDependenciesKey, parentKey, parentId, timestamp)
-    local isParentActive = rcall("ZSCORE", parentQueueKey .. ":waiting-children", parentId)
-    if isParentActive and rcall("SCARD", parentDependenciesKey) == 0 then
+    local hasPendingDependencies = rcall("SCARD", parentDependenciesKey) > 0
+    if hasPendingDependencies then
         moveParentToWait(parentQueueKey, parentKey, parentId, timestamp)
     end
 end
