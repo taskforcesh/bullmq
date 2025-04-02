0a9fa385a24791f8319bb0e4fdba0716c9bfafe5
Manuel Astudillo <manuel@optimalbits.com>
2025-04-02 16:31:48 +0200

    refactor: refactor remove job command

--- src/commands/removeJob-3.lua
+++ src/commands/removeJob-3.lua
@@ -13,79 +13,13 @@
     Events:
       'removed'
 ]]
-
+      
 local rcall = redis.call
 
 -- Includes
---- @include "includes/destructureJobKey"
---- @include "includes/getOrSetMaxEvents"
 --- @include "includes/isJobSchedulerJob"
 --- @include "includes/isLocked"
---- @include "includes/removeDeduplicationKey"
---- @include "includes/removeJobFromAnyState"
---- @include "includes/removeJobKeys"
---- @include "includes/removeParentDependencyKey"
-
-local function removeJob(prefix, jobId, parentKey, removeChildren)
-    local jobKey = prefix .. jobId;
-
-    removeParentDependencyKey(jobKey, false, parentKey, nil)
-
-    if removeChildren == "1" then
-        -- Check if this job has children
-        -- If so, we are going to try to remove the children recursively in deep first way because
-        -- if some job is locked we must exit with and error.
-        -- local countProcessed = rcall("HLEN", jobKey .. ":processed")
-        local processed = rcall("HGETALL", jobKey .. ":processed")
-
-        if (#processed > 0) then
-            for i = 1, #processed, 2 do
-                local childJobId = getJobIdFromKey(processed[i])
-                local childJobPrefix = getJobKeyPrefix(processed[i], childJobId)
-                removeJob(childJobPrefix, childJobId, jobKey, removeChildren)
-            end
-        end
-
-        local dependencies = rcall("SMEMBERS", jobKey .. ":dependencies")
-        if (#dependencies > 0) then
-            for i, childJobKey in ipairs(dependencies) do
-                -- We need to get the jobId for this job.
-                local childJobId = getJobIdFromKey(childJobKey)
-                local childJobPrefix = getJobKeyPrefix(childJobKey, childJobId)
-                removeJob(childJobPrefix, childJobId, jobKey, removeChildren)
-            end
-        end
-
-        local failed = rcall("HGETALL", jobKey .. ":failed")
-
-        if (#failed > 0) then
-            for i = 1, #failed, 2 do
-                local childJobId = getJobIdFromKey(failed[i])
-                local childJobPrefix = getJobKeyPrefix(failed[i], childJobId)
-                removeJob(childJobPrefix, childJobId, jobKey, removeChildren)
-            end
-        end
-
-        local unsuccessful = rcall("ZRANGE", jobKey .. ":unsuccessful", 0, -1)
-
-        if (#unsuccessful > 0) then
-            for i = 1, #unsuccessful, 1 do
-                local childJobId = getJobIdFromKey(unsuccessful[i])
-                local childJobPrefix = getJobKeyPrefix(unsuccessful[i], childJobId)
-                removeJob(childJobPrefix, childJobId, jobKey, removeChildren)
-            end
-        end
-    end
-
-    local prev = removeJobFromAnyState(prefix, jobId)
-
-    removeDeduplicationKey(prefix, jobKey)
-    if removeJobKeys(jobKey) > 0 then
-        local maxEvents = getOrSetMaxEvents(KEYS[2])
-        rcall("XADD", prefix .. "events", "MAXLEN", "~", maxEvents, "*", "event", "removed", "jobId", jobId, "prev",
-            prev)
-    end
-end
+--- @include "includes/removeJobWithChildren"
 
 local prefix = KEYS[1]
 local jobId = ARGV[1]
@@ -98,7 +32,7 @@ if isJobSchedulerJob(jobId, jobKey, repeatKey) then
 end
 
 if not isLocked(prefix, jobId, shouldRemoveChildren) then
-    removeJob(prefix, jobId, nil, shouldRemoveChildren)
+    removeJobWithChildren(prefix, jobId, nil, shouldRemoveChildren)
     return 1
 end
 return 0
