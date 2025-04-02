90a96e1b6c3b974972130bc77068037ad4a45c07
Andrea <nullndr@proton.me>
2025-04-02 10:38:24 +0200

    chore: use `JobProgress` type for sandboxed job

--- src/interfaces/sandboxed-job.ts
+++ src/interfaces/sandboxed-job.ts
@@ -1,4 +1,4 @@
-import { JobJsonSandbox, JobsOptions } from '../types';
+import { JobJsonSandbox, JobProgress, JobsOptions } from '../types';
 
 /**
  * @see {@link https://docs.bullmq.io/guide/workers/sandboxed-processors}
@@ -10,6 +10,6 @@ export interface SandboxedJob<T = any, R = any>
   moveToDelayed: (timestamp: number, token?: string) => Promise<void>;
   log: (row: any) => void;
   updateData: (data: any) => Promise<void>;
-  updateProgress: (value: object | number | string | boolean) => Promise<void>;
+  updateProgress: (value: JobProgress) => Promise<void>;
   returnValue: R;
 }
