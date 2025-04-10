8456cb5ff2da04f2a2e400e844fa8ddad0dd1823
Rogger Taskforce <roggertaskforce@Roggers-MacBook-Air.local>
2025-04-09 19:00:42 -0600

    refactor: extract common method

--- src/commands/addJobScheduler-11.lua
+++ src/commands/addJobScheduler-11.lua
@@ -59,81 +59,50 @@ local schedulerKey = repeatKey .. ":" .. jobSchedulerId
 local nextDelayedJobKey = schedulerKey .. ":" .. nextMillis
 local nextDelayedJobId = "repeat:" .. jobSchedulerId .. ":" .. nextMillis
 
-if rcall("EXISTS", nextDelayedJobKey) == 1 then
-    if rcall("ZSCORE", delayedKey, nextDelayedJobId) then
+local maxEvents = getOrSetMaxEvents(metaKey)
+
+local function removeJobFromScheduler(prefixKey, delayedKey, prioritizedKey, waitKey, pausedKey, jobId, metaKey, eventsKey)
+    if rcall("ZSCORE", delayedKey, jobId) then
         removeJob(nextDelayedJobId, true, prefixKey, true --[[remove debounce key]] )
-        rcall("ZREM", delayedKey, nextDelayedJobId)
-    elseif rcall("ZSCORE", prioritizedKey, nextDelayedJobId) then
-        removeJob(nextDelayedJobId, true, prefixKey, true --[[remove debounce key]] )
-        rcall("ZREM", prioritizedKey, nextDelayedJobId)
+        rcall("ZREM", delayedKey, jobId)
+        return true
+    elseif rcall("ZSCORE", prioritizedKey, jobId) then
+        removeJob(jobId, true, prefixKey, true --[[remove debounce key]] )
+        rcall("ZREM", prioritizedKey, jobId)
+        return true
     else
         local pausedOrWaitKey = waitKey
         if isQueuePaused(metaKey) then
             pausedOrWaitKey = pausedKey
         end
 
-        if rcall("LREM", pausedOrWaitKey, 1, nextDelayedJobId) > 0 then
-            removeJob(nextDelayedJobId, true, prefixKey, true --[[remove debounce key]] )
-        else
-            local maxEvents = getOrSetMaxEvents(metaKey)
-
-            rcall("XADD", eventsKey, "MAXLEN", "~", maxEvents, "*", "event",
-                "duplicated", "jobId", nextDelayedJobId)
-
-            return nextDelayedJobId .. "" -- convert to string
+        if rcall("LREM", pausedOrWaitKey, 1, jobId) > 0 then
+            removeJob(jobId, true, prefixKey, true --[[remove debounce key]] )
+            return true
         end
+    end
+    return false
+end
+    
+if rcall("EXISTS", nextDelayedJobKey) == 1 then
+    if not removeJobFromScheduler(prefixKey, delayedKey, prioritizedKey, waitKey, pausedKey,
+        nextDelayedJobId, metaKey, eventsKey) then
+        rcall("XADD", eventsKey, "MAXLEN", "~", maxEvents, "*", "event",
+            "duplicated", "jobId", nextDelayedJobId)
+
+        return nextDelayedJobId .. "" -- convert to string
     end
 end
 
 local prevMillis = rcall("ZSCORE", repeatKey, jobSchedulerId)
 
-if prevMillis ~= false then
+if prevMillis then
     local currentJobId = "repeat:" .. jobSchedulerId .. ":" .. prevMillis
     local currentDelayedJobKey = schedulerKey .. ":" .. prevMillis
-
-    if rcall("EXISTS", nextDelayedJobKey) == 1 then
-        if rcall("ZSCORE", delayedKey, nextDelayedJobId) then
-            removeJob(nextDelayedJobId, true, prefixKey, true --[[remove debounce key]] )
-            rcall("ZREM", delayedKey, nextDelayedJobId)
-        elseif rcall("ZSCORE", prioritizedKey, nextDelayedJobId) then
-            removeJob(nextDelayedJobId, true, prefixKey, true --[[remove debounce key]] )
-            rcall("ZREM", prioritizedKey, nextDelayedJobId)
-        else
-            local pausedOrWaitKey = waitKey
-            if isQueuePaused(metaKey) then
-                pausedOrWaitKey = pausedKey
-            end
-    
-            if rcall("LREM", pausedOrWaitKey, 1, nextDelayedJobId) > 0 then
-                removeJob(nextDelayedJobId, true, prefixKey, true --[[remove debounce key]] )
-            else
-                local maxEvents = getOrSetMaxEvents(metaKey)
-    
-                rcall("XADD", eventsKey, "MAXLEN", "~", maxEvents, "*", "event",
-                    "duplicated", "jobId", nextDelayedJobId)
-    
-                return nextDelayedJobId .. "" -- convert to string
-            end
-        end
-    end
     
     if currentJobId ~= nextDelayedJobId and rcall("EXISTS", currentDelayedJobKey) == 1 then
-        if rcall("ZSCORE", delayedKey, currentJobId) ~= false then
-            removeJob(currentJobId, true, prefixKey, true --[[remove debounce key]] )
-            rcall("ZREM", delayedKey, currentJobId)
-        elseif rcall("ZSCORE", prioritizedKey, currentJobId) ~= false then
-            removeJob(currentJobId, true, prefixKey, true --[[remove debounce key]] )
-            rcall("ZREM", prioritizedKey, currentJobId)
-        else
-            local pausedOrWaitKey = waitKey
-            if isQueuePaused(metaKey) then
-                pausedOrWaitKey = pausedKey
-            end
-
-            if rcall("LREM", pausedOrWaitKey, 1, currentJobId) > 0 then
-                removeJob(currentJobId, true, prefixKey, true --[[remove debounce key]] )
-            end
-        end
+        removeJobFromScheduler(prefixKey, delayedKey, prioritizedKey, waitKey, pausedKey,
+            currentJobId, metaKey, eventsKey)
     end
 end
 
@@ -141,8 +110,6 @@ local schedulerOpts = cmsgpack.unpack(ARGV[2])
 storeJobScheduler(jobSchedulerId, schedulerKey, repeatKey, nextMillis, schedulerOpts, ARGV[4], templateOpts)
 
 rcall("INCR", KEYS[8])
-
-local maxEvents = getOrSetMaxEvents(metaKey)
 
 addJobFromScheduler(nextDelayedJobKey, nextDelayedJobId, ARGV[6], waitKey, pausedKey,
     KEYS[11], metaKey, prioritizedKey, KEYS[10], delayedKey, KEYS[7], eventsKey,
