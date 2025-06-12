fe963c945e2d5e2ca0f99c8ff6580eba2ab9c2a0
roggervalf <rogger.valvrd@gmail.com>
2025-06-11 22:56:41 -0600

    fix: avoid circular reference between scripts and queue

--- src/classes/queue-base.ts
+++ src/classes/queue-base.ts
@@ -89,7 +89,14 @@ export class QueueBase extends EventEmitter implements MinimalQueue {
   }
 
   protected setScripts() {
-    this.scripts = new Scripts(this);
+    this.scripts = new Scripts({
+      keys: this.keys,
+      client: this.client,
+      redisVersion: this.redisVersion,
+      toKey: this.toKey,
+      opts: this.opts,
+      closing: this.closing,
+    });
   }
 
   /**
