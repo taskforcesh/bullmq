2fe27172b8d3736fe6dda329b11291155ef14dc0
roggervalf <rogger.valvrd@gmail.com>
2025-06-12 19:05:05 -0600

    test: fix test case

--- python/tests/worker_test.py
+++ python/tests/worker_test.py
@@ -168,7 +168,7 @@ class TestWorker(unittest.IsolatedAsyncioTestCase):
         data = {"foo": "bar"}
         job = await queue.add("test-job", data, {"removeOnComplete": False})
 
-        failedReason = "Out of range float values are not JSON compliant: nan"
+        failedReason = "Out of range float values are not JSON compliant"
 
         async def process(job: Job, token: str):
             print("Processing job", job)
