278c783445a3464ecd89f87449c883603257116e
roggervalf <rogger.valvrd@gmail.com>
2025-07-18 22:12:07 -0600

    refactor: return error earlier

--- src/commands/moveJobFromActiveToWait-9.lua
+++ src/commands/moveJobFromActiveToWait-9.lua
@@ -29,39 +29,39 @@ local jobId = ARGV[1]
 local token = ARGV[2]
 local jobKey = ARGV[3]
 
-if rcall("EXISTS", jobKey) == 1 then
-  local errorCode = removeLock(jobKey, KEYS[3], token, jobId)
-  if errorCode < 0 then
-    return errorCode
-  end
-
-  local metaKey = KEYS[5]
-  local removed = rcall("LREM", KEYS[1], 1, jobId)
-  if removed > 0 then
-    local target, isPausedOrMaxed = getTargetQueueList(metaKey, KEYS[1], KEYS[2], KEYS[4])
-
-    local priority = tonumber(rcall("HGET", ARGV[3], "priority")) or 0
-
-    if priority > 0 then
-      pushBackJobWithPriority(KEYS[7], priority, jobId)
-    else
-      addJobInTargetList(target, KEYS[8], "RPUSH", isPausedOrMaxed, jobId)
-    end
-
-    local maxEvents = getOrSetMaxEvents(metaKey)
-
-    -- Emit waiting event
-    rcall("XADD", KEYS[9], "MAXLEN", "~", maxEvents, "*", "event", "waiting",
-      "jobId", jobId, "prev", "active")
-  end
-
-  local pttl = rcall("PTTL", KEYS[6])
-
-  if pttl > 0 then
-    return pttl
-  else
-    return 0
-  end
-else
+if rcall("EXISTS", jobKey) == 0 then
   return -1
+end
+
+local errorCode = removeLock(jobKey, KEYS[3], token, jobId)
+if errorCode < 0 then
+  return errorCode
+end
+
+local metaKey = KEYS[5]
+local removed = rcall("LREM", KEYS[1], 1, jobId)
+if removed > 0 then
+  local target, isPausedOrMaxed = getTargetQueueList(metaKey, KEYS[1], KEYS[2], KEYS[4])
+
+  local priority = tonumber(rcall("HGET", ARGV[3], "priority")) or 0
+
+  if priority > 0 then
+    pushBackJobWithPriority(KEYS[7], priority, jobId)
+  else
+    addJobInTargetList(target, KEYS[8], "RPUSH", isPausedOrMaxed, jobId)
+  end
+
+  local maxEvents = getOrSetMaxEvents(metaKey)
+
+  -- Emit waiting event
+  rcall("XADD", KEYS[9], "MAXLEN", "~", maxEvents, "*", "event", "waiting",
+    "jobId", jobId, "prev", "active")
+end
+
+local pttl = rcall("PTTL", KEYS[6])
+
+if pttl > 0 then
+  return pttl
+else
+  return 0
 end
