d516727c49f55e3db2930d1b8bb9b4a795d883ed
roggervalf <rogger.valvrd@gmail.com>
2025-07-20 22:21:28 -0600

    refactor: extract removal of job from active as a reusable method

--- src/commands/moveToWaitingChildren-8.lua
+++ src/commands/moveToWaitingChildren-8.lua
@@ -32,6 +32,7 @@ local jobDependenciesKey = KEYS[4]
 local jobUnsuccessfulKey = KEYS[5]
 local stalledKey = KEYS[6]
 local failedKey = KEYS[7]
+local token = ARGV[1]
 local timestamp = ARGV[3]
 local jobId = ARGV[4]
 
@@ -41,15 +42,30 @@ local jobId = ARGV[4]
 --- @include "includes/removeJobsOnFail"
 --- @include "includes/removeLock"
 
-local function moveToWaitingChildren(activeKey, waitingChildrenKey, jobId,
-    timestamp)
-  local score = tonumber(timestamp)
+local function removeJobFromActive(activeKey, stalledKey, jobKey, jobId,
+    token)
+  local errorCode = removeLock(jobKey, stalledKey, token, jobId)
+  if errorCode < 0 then
+    return errorCode
+  end
 
   local numRemovedElements = rcall("LREM", activeKey, -1, jobId)
 
   if(numRemovedElements < 1) then
     return -3
   end
+
+  return 0
+end
+
+local function moveToWaitingChildren(activeKey, waitingChildrenKey, stalledKey, jobKey, jobId,
+    timestamp, token)
+  local errorCode = removeJobFromActive(activeKey, stalledKey, jobKey, jobId, token)
+  if errorCode < 0 then
+    return errorCode
+  end
+
+  local score = tonumber(timestamp)
 
   rcall("ZADD", waitingChildrenKey, score, jobId)
 
@@ -58,15 +74,9 @@ end
 
 if rcall("EXISTS", jobKey) == 1 then
   if rcall("ZCARD", jobUnsuccessfulKey) ~= 0 then
-    local errorCode = removeLock(jobKey, stalledKey, ARGV[1], jobId)
+    local errorCode = removeJobFromActive(activeKey, stalledKey, jobKey, jobId, token)
     if errorCode < 0 then
       return errorCode
-    end
-
-    local numRemovedElements = rcall("LREM", activeKey, 1, jobId)
-
-    if numRemovedElements < 1 then
-      return -3
     end
 
     -- TODO: refactor this logic in an include later
@@ -92,21 +102,15 @@ if rcall("EXISTS", jobKey) == 1 then
   else
     if ARGV[2] ~= "" then
       if rcall("SISMEMBER", jobDependenciesKey, ARGV[2]) ~= 0 then
-        local errorCode = removeLock(jobKey, stalledKey, ARGV[1], jobId)
-        if errorCode < 0 then
-          return errorCode
-        end
-        return moveToWaitingChildren(activeKey, waitingChildrenKey, jobId, timestamp)
+        return moveToWaitingChildren(activeKey, waitingChildrenKey, stalledKey, jobKey, jobId,
+          timestamp, token)
       end
   
       return 1
     else
       if rcall("SCARD", jobDependenciesKey) ~= 0 then 
-        local errorCode = removeLock(jobKey, stalledKey, ARGV[1], jobId)
-        if errorCode < 0 then
-          return errorCode
-        end
-        return moveToWaitingChildren(activeKey, waitingChildrenKey, jobId, timestamp)
+        return moveToWaitingChildren(activeKey, waitingChildrenKey, stalledKey, jobKey, jobId,
+          timestamp, token)
       end
   
       return 1
