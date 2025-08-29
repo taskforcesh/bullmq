998d0c02b3fd43f2f2e0804f5e623a3ca560a848
Yazan Medanat <medanat@gmail.com>
2025-08-29 16:36:07 -0400

    fix(worker): add missing processor type export

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,4 +6,5 @@ export * from './job-options';
 export * from './job-scheduler-template-options';
 export * from './job-type';
 export * from './job-progress';
+export * from './processor';
 export * from './repeat-strategy';
