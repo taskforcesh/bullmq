380c0409c12a9955714ede158f6576be72fbbf30
yogeshwaran-c <ycyogesh183@gmail.com>
2026-04-15 08:04:07 +0530

    docs(python): add docstrings to PromoteJobsOptions and RetryJobsOptions fields

--- python/bullmq/types/promote_jobs_options.py
+++ python/bullmq/types/promote_jobs_options.py
@@ -3,4 +3,11 @@ from typing import TypedDict
 
 
 class PromoteJobsOptions(TypedDict, total=False):
+    """
+    Options for the promoteJobs method.
+    """
+
     count: int
+    """
+    Maximum number of jobs to promote.
+    """
