68e7606b86e2f2d969255fa873ad3998f05530f8
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2026-09-10 09:47:12 -0300

    fix(backends): keep is_maxed for backward compatibility [elixir]

--- elixir/lib/bullmq/scripts.ex
+++ elixir/lib/bullmq/scripts.ex
@@ -1838,6 +1838,15 @@ defmodule BullMQ.Scripts do
     execute(conn, :is_maxed, keys, args)
   end
 
+  @deprecated "Use maxed?/2 instead"
+  @spec is_maxed(pid(), String.t()) :: {:ok, boolean()} | {:error, term()}
+  def is_maxed(conn, ctx) do
+    case maxed?(conn, ctx) do
+      true -> {:ok, true}
+      false -> {:error, false}
+    end
+  end
+
   @doc """
   Gets the rate limit TTL.
 
